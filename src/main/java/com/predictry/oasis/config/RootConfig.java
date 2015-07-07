e1ecd7164d12dc2c57b3621cea38a3bb9832b6a3
Jocki <jocki.predictry@gmail.com>
2015-07-07 15:03:24 +0800

    First commit in Java.

---
+++ src/main/java/com/predictry/oasis/config/RootConfig.java
@@ -0,0 +1,14 @@
+package com.predictry.oasis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring root configuration.
+ * 
+ * @author jocki
+ *
+ */
+@Configuration
+public class RootConfig {
+
+}
